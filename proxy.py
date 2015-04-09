60e47d5ca6cb3bd9ded3dfa438a2ef4720080503
Nate Matthews <natezmatthews@gmail.com>
2015-04-09 12:35:34 -0400

    Moved linkfinder to its own function

--- proxy.py
+++ proxy.py
@@ -3,51 +3,37 @@ import time
 import sys
 import select
 
-NUM_CLIENTS = 5
+NUM_CLIENTS = 1
 BUFFER_SIZE = 4096
-
-# class Forward:
-#     def __init__(self):
-#         self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- 
-#     def start(self, host, port):
-#         try:
-#             self.forward.connect((host, port))
-#             return self.forward
-#         except Exception, e:
-#             print e
-#             return False
-
 
 class Server:
     def __init__(self, host, port):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     #   self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((host, port))
         self.server.listen(NUM_CLIENTS)
-        print "listening in on port ", port
-
 
     def readFromClient(self):
-        print "hello"
         cli_sockfd, cli_addr = self.server.accept()
-        print cli_addr, "has connected"
         self.data = cli_sockfd.recv(BUFFER_SIZE)
         return self.data
+
+def findLinks(document):
+    linkstr = "<a href="
+    startind = document.find(linkstr) + len(linkstr)
+    endind = document.find(document[startind], startind + 1)
+    return document[(startind + 1):endind]
 
 if __name__ == '__main__':
     try:
         if len(sys.argv) != 3:
-            print "Please specify port number"
+            print "Usage: " + sys.argv[0] + "[hostname] [port number]"
             sys.exit(1)
         hostname = sys.argv[1]
         portno = int(sys.argv[2])
-        print hostname, portno
         server = Server(hostname, portno)
         req = server.readFromClient()
         elems = req.split()
-        for a in elems:
-            print a
+
         dest = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             dest.connect((elems[4], 80))
@@ -57,15 +43,10 @@ if __name__ == '__main__':
         resp = ''
         while 1:
             read = dest.recv(BUFFER_SIZE)
-            # if read.find("</html>"):
-            #    break
             if len(read) == 0:
                 break
             resp += read
-            print resp
-        linkstr = "<a href="
-        startind = resp.find(linkstr) + len(linkstr)
-        endind = resp.find(resp[startind], startind + 1)
+        print findLinks(resp)
 
     except KeyboardInterrupt:
         print "Ctrl C - Stopping server"
